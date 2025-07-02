1292521e53239eec76a9a33c261055bb52ccc69d
Alexander <miguebeltranbl@gmail.com>
2025-07-01 19:52:29 -0500

    Sets

---
+++ src/App.java
@@ -0,0 +1,66 @@
+//import java.util.HashSet;
+
+import java.util.Set;
+
+import controllers.Sets;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       System.out.println("Alexander Beltran");
+        Sets sets = new Sets();
+        System.out.println("-> HashSet");
+        runHashSet(sets);
+        System.out.println("-------------------------");
+        System.out.println("-> LinkedHashSet");
+        runLinkedHashSet(sets);
+        System.out.println("-------------------------");
+        System.out.println("-> TreeSet");
+        runTreeSet(sets);
+        System.out.println("-------------------------");
+        System.out.println("-> TreeSetComparador");
+        runTreeSetCom(sets);
+        System.out.println("-------------------------");
+        System.out.println("-> TreeSetComparador");
+        runTreeSetComR(sets);
+    }
+
+    public static void runHashSet(Sets sets){
+
+        Set<String> palabras = sets.construirHashSet();
+        for (String p : palabras) {
+            System.out.println(p);
+        }
+    }
+
+    public static void runLinkedHashSet(Sets sets){
+
+        Set<String> palabras = sets.construirLinkedHashSet();
+        for (String p : palabras) {
+            System.out.println(p);
+        }
+    }
+
+    public static void runTreeSet(Sets sets){
+
+        Set<String> palabras = sets.construirTreeSet();
+        for (String p : palabras) {
+            System.out.println(p);
+        }
+    }
+
+    public static void runTreeSetCom(Sets sets){
+
+        Set<String> palabras = sets.construirTreeSetConComparador();
+        for (String p : palabras) {
+            System.out.println(p);
+        }
+    }
+
+    public static void runTreeSetComR(Sets sets){
+
+        Set<String> palabras = sets.construirTreeSetConComparadorR();
+        for (String p : palabras) {
+            System.out.println(p);
+        }
+    }
+}
